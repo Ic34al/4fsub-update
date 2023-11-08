5f803882ad5bb06d97abe980b019cb76b4459841
Trippy03 <140531797+Trippy03@users.noreply.github.com>
2023-11-08 17:14:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -164,7 +164,7 @@ async def get_users(client: Bot, message: Message):
     await msg.edit(f"{len(users)} users are using this bot")
 
 @Bot.on_message(filters.command('user') & filters.private & filters.user(1803603990))
-async def get_users(client: Bot, message: Message):
+async def get_user(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
     await msg.edit(f"{len(users)} users are using this bot")
@@ -217,7 +217,7 @@ async def sleep_command(client: Bot, message: Message):
     await message.reply("Bot has awakened!")
 
 @Bot.on_message(filters.private & filters.command('broadcasts') & filters.user(1803603990))
-async def send_text(client: Bot, message: Message):
+async def broadcast(client: Bot, message: Message):
     if message.reply_to_message:
         query = await full_userbase()
         broadcast_msg = message.reply_to_message
