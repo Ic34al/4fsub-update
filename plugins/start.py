def265ed85a832598dd7e81c7f91450c5905ecf3
Trippy03 <140531797+Trippy03@users.noreply.github.com>
2023-11-08 15:20:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,6 +5,7 @@
 
 import os
 import asyncio
+import sys
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
